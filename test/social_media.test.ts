7637a7d4dd51fbe78b50f4f2a7ffaaf5d3946f16
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2022-12-19 13:00:15 +0500

    Initial commit

---
+++ test/social_media.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SocialMedia from '../lib/social_media-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/social_media-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SocialMedia.SocialMediaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
